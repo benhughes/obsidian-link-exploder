a806b841aca7b24087712df4cef9ff9f40abb8ab
unex <63149623+UNEXENU@users.noreply.github.com>
2023-07-21 13:43:31 +0200

    Fixing getFileName not supporting dots in path

--- lib/canvas/canvas.ts
+++ lib/canvas/canvas.ts
@@ -294,7 +294,9 @@ const getFileName = (
     return path;
   }
   const limit = 50;
-  const [name, extention] = path.split('.');
+  const idx = path.lastIndexOf('.');
+  const name = path.substring(0, idx);
+  const extention = path.substring(idx);
   for (let i = 0; i < limit; i++) {
     const newPath = `${name}-${i}.${extention}`;
     log.info(newPath);
